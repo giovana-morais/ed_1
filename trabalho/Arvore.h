449f4ebc29eafd1e73c23d0095286bfefd1ec6d7
Giovana Morais <giovana.vmorais@gmail.com>
2016-11-19 15:36:07 -0200

    atualização do trabalho

--- trabalho/Arvore.h
+++ trabalho/Arvore.h
@@ -17,14 +17,15 @@ typedef struct tree {
 FILE *arquivo;
 void inicializaArvore(tree **xmas_tree);
 int insereRA(tree **ra_tree, Aluno *aluno);
-void insereNome(tree **nome_tree, Aluno *aluno);
+int insereNome(tree **nome_tree, Aluno *aluno);
 tree* buscaRA(tree *ra_tree, int ra);
 tree* buscaNome(tree *nome_tree, char *busca, int busca_suja);
-// int listarNome(tree *nome_tree, char *nome_busca, int busca_suja); /* <-- imprimirAluno vai dentro*/
+int listarNome(tree *nome_tree, char *nome_busca, int busca_suja); /* <-- imprimirAluno vai dentro*/
 void liberaArvore(tree **xmas_tree);
 void salvarArvore(tree *xmas_tree, FILE *arquivo);
 void carregarArvore(tree **xmas_tree,FILE* arquivo);
 void imprimirAluno(Aluno aluno);
-
+// remover essa função depois
+void inorder(tree **p);
 
 #endif
